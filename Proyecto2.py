2ea439b30f30c34cf346a4364456dc9f9c95dcfc
dianaxime <35496688+dianaxime@users.noreply.github.com>
2019-05-20 16:03:22 -0600

    Add files via upload

--- Proyecto2.py
+++ Proyecto2.py
@@ -8,7 +8,7 @@
 #Documentacion extraida de https://neo4j-rest-client.readthedocs.io/en/latest/
 
 from neo4jrestclient.client import GraphDatabase
-gdb = GraphDatabase("http://localhost:7474", username="neo4j", password="12345")
+gdb = GraphDatabase("http://localhost:7474", username="neo4j", password="1111")
 
 def addPersona(nombre):
     persona= gdb.nodes.create(name=nombre)
@@ -50,3 +50,21 @@ def addActividad (nombre, actividades):
     persona.get(name=nombre)[0].relationships.create("tiempolibre",actividad.get(hacer=actividades)[0])
 
 #-------------------------------------------------------------------------------------------------------------------------------------------
+
+def getPersona(nombre):
+    buscar="match (persona{name:'"+nombre+"'})-[x]->(d) return x,d"
+    resultados = gdb.query(buscar, data_contents=True)
+    listado=resultados.rows
+    listado = resultados.rows
+    loficial=[]
+    for x in listado:  
+        for i in x: 
+            for c in i:
+                if c is not None:
+                    loficial.append(i[c])
+    return loficial
+
+
+
+nombre=input("Ingrese un nombre completo")
+print(getPersona(nombre))
