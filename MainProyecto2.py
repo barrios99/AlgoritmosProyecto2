0cc31371dc4a3cd9c698df6e4d3fbf47c3931261
Fátima Albeño <35973337+barrios99@users.noreply.github.com>
2019-05-23 09:02:36 -0600

    Add files via upload

--- MainProyecto2.py
+++ MainProyecto2.py
@@ -30,6 +30,24 @@ while (opcion != 3):
         #Agrega una nueva persona a la base de datos
         nombre = input("Ingrese su nombre completo: ")
         #addPersona(nombre)
+ 
+        #while ((ocupacion != 1) or (ocupacion != 2) or (ocupacion != 3)):
+        ocupacion = input("\nIngrese '1' si estudia, ingrese '2' si trabaja o ingrese '3' si ambas: ")
+        validar = verificarNum(ocupacion)
+        while validar == False:
+            print("El dato ingresado es invalido.")
+            ocupacion = input("\nIngrese '1' si estudia, ingrese '2' si trabaja o ingrese '3' si ambas: ")
+            validar = verificarNum(ocupacion)
+        if validar == True:
+            if ocupacion == 1:
+                e = "Estudia"
+                #addOcupacion(nombre,e)
+            if ocupacion == 2:
+                t = "Trabaja"
+                #addOcupacion(nombre,t)
+            if ocupacion == 3:
+                et = "Estudia y trabaja"
+                #addOcupacion(nombre,et)
 
         #Agrega el tipo de personalidad de la persona
         tipos = ["\n0. Pacificador", "1. Reformador", "2. Triunfador", "3. Ayudador", "4. Romántico", "5. Investigador", "6. Leal", "7. Entusiasta", "8. Desafiador", "9. Extrovertido", "10. Introvertido", "11. Ninguna de las anteriores \n"]
@@ -52,7 +70,7 @@ while (opcion != 3):
                 
         #Agregar lo que valora en una amistad 
         else:
-            valor = ["\n0. Confianza", "1. Lealtad","2. Honestidad","3. Apoyo mutuo","4. Humildad","5. Solidaridad","6. Compañerismo","7. Tolerancia \n"]
+            valor = ["\n0. Confianza", "1. Lealtad","2. Honestidad","3. Apoyo mutuo","4. Humildad","5. Solidaridad","6. Compañerismo","7. Tolerancia","8. Ninguna de las anteriores. \n"]
             for i in valor:
                 print(i)
             amis = []
@@ -66,8 +84,11 @@ while (opcion != 3):
                     validar = verificarNum(amistad)
                 if validar == True:
                     parar = input("¿Desea agregar otro valor para amistad? \nResponda 'n' para parar, o cualquier otra tecla para continuar")
-
+                    amis.append(i)
+                #for i in amis:
+                    #addAmistad(nombre, i)
+                    
             #Agregar sus generos musicales preferidos
+            generos = ["0. Disco", "1. Reggaeton", "2. Salsa","3. Pop","4. Rock","5. Electrónica","6. Rap","7. Trap","8. Hip-Hop","9. Cristiana","10. Jazz","11. Metal","12. Cumbia", "13. Merengue \n"]
             musica = input("¿Cuáles son sus géneros musicales faovritos?")
-            ocupacion = input("Ingrese 0 si estudia, ingrese 1 si trabaja o ingrese 3 si ambas: ")
             actividad = input("¿Qué actividades realiza en su tiempo libre?")
